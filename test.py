c6008f55bba33d54c7034dd6690508505cf211c7
Anton <anton-qz@yandex.ru>
2026-03-18 15:15:34 +0700

    RGA

--- test.py
+++ test.py
@@ -8,21 +8,15 @@ time1 = []
 time2 = []
 p1 = []
 p2 = []
-c_len = [467,460,462,461,462,466,463,463,465,464,463,466,461,461,461,463,465,461,461,462,463,461,462,461,463,464,466,464,466,468,466,453,364,338,342]
+c_len = []
+with open(f"cycle.txt", "r") as f:
+    for line in f.readlines():
+        c_len.append(float(line))
 c_len = c_len[::-1]
-# with open(f'КЦА_20_циклограмма.csv', mode='r', newline='') as file:
-#     csv_reader = csv.reader(file, delimiter=";")
-#     next(csv_reader, None) 
-#     for row in csv_reader:
-#         if row[1] != "":
-#             time1.append(float(row[0].replace(",", ".")))
-#             p1.append(row[1])
-#         if row[2] != "":
-#             time2.append(float(row[2].replace(",", ".")))
-#             p2.append(row[3])
+
 
 time1 = []
-with open(f"time_PLK.txt", "r") as f:
+with open(f"timeline.txt", "r") as f:
     for line in f.readlines():
         time1.append(float(line))
 
@@ -45,10 +39,10 @@ for i in range(len(time1)):
 
 
 with open('циклограмма.csv', "w", newline="", encoding="utf-8") as f:
-    writer = csv.writer(f, delimiter=";")
+    writer = csv.writer(f, delimiter=" ")
     for i in range(len(res1)):
         row = []
         if i < len(res1):
-            row.append(f"{time1[i]}".replace('.', ','))
-            row.append(f"{res1[i]}".replace('.', ','))
+            row.append(f"{time1[i]+t0}")# .replace('.', ','))
+            row.append(f"{res1[i]}" )#.replace('.', ','))
         writer.writerow(row)
